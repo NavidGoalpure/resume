b64810bd038e74ecbe9b771538e624ca893bb290
NavidGoalpure <navidgoalpure@gmail.com>
2020-01-05 18:36:51 +0330

    initial redux config

--- src/pages/SkillsTour.js
+++ src/pages/SkillsTour.js
@@ -2,17 +2,26 @@
 import React from 'react';
 import ShowImages from '../components/skillsTour/shopping/ShowImages'
 import Layout from '../components/Layout';
+import {connect} from "react-redux"
+import {toggleDarkMode} from "../state/app"
 
-const IndexPage = () => (
+const IndexPage = ({isDarkMode, dispatch}) => (
   <Layout>
     <section id="one" className="main-container">
       <div className="container">
         <div className="content">
           <ShowImages />
+          <button
+            style={isDarkMode ? {backgroundColor: 'black', color: 'white'} : null}
+            onClick={() => dispatch(toggleDarkMode(!isDarkMode))}> Dark Mode {isDarkMode ? 'on' : 'off'}
+          </button>
+
         </div>
       </div>
     </section>
   </Layout>
 );
 
-export default IndexPage;
+export default connect(state => ({
+  isDarkMode: state.app.isDarkMode
+}), null)(IndexPage)
