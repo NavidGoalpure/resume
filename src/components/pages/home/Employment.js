95a9166e4f809bddfe03950eb2e37aceecc8f919
Navid <navid.goalpure@gmail.com>
2020-01-26 22:38:23 +0330

    remove unusable text

--- src/components/pages/home/Employment.js
+++ src/components/pages/home/Employment.js
@@ -1,7 +1,6 @@
-
-import React from "react"
-import config from "../../../assets/ComponentsData"
-import styled from "styled-components"
+import React from 'react'
+import config from '../../../assets/ComponentsData'
+import styled from 'styled-components'
 
 const Section = styled.section`
   margin-top: 0 !important;
@@ -11,7 +10,7 @@ const Row = styled.p`
 `
 const Date = styled.h3`
   margin-bottom: 0.5rem;
-    @media (max-width: 1150px) {
+  @media (max-width: 1150px) {
     color: darkgreen;
     text-decoration: underline;
   }
@@ -20,30 +19,28 @@ function Employment() {
   const content = config.employment.map((job, i) => (
     <Section key={i}>
       <header>
-        <Date >{job.duration}</Date>
-
+        <Date>{job.duration}</Date>
       </header>
-      <div className="content" style={{marginBottom: '2rem'}}>
-        <Row ><strong>{job.title}</strong>-{job.companyName}</Row>
+      <div className="content" style={{ marginBottom: '2rem' }}>
+        <Row>
+          <strong>{job.title}</strong>-{job.companyName}
+        </Row>
         {job.characters.map((character, i) => (
           <Row key={i}>+ {character}</Row>
-        )
-        )}
+        ))}
       </div>
     </Section>
-
-  )
-  )
+  ))
   return (
     <section id="JobsContainer">
       <header>
         <h2>Employment</h2>
       </header>
       <div className="content">
-        <p>
+        {/* <p>
           <strong>Integer mollis egestas</strong> nam maximus erat id euismod
           egestas. Pellentesque sapien ac quam. Lorem ipsum dolor sit nullam.
-          </p>
+          </p> */}
         {content}
       </div>
     </section>
