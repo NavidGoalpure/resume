a8505fccd7fb5dd85d06488e27a63d60359fdac0
Navid <navid.goalpure@gmail.com>
2020-01-27 14:46:10 +0330

    change manifest Color

--- config.js
+++ config.js
@@ -3,8 +3,8 @@ module.exports = {
   manifestName: 'Navid Resume',
   manifestShortName: 'Resume', // max 12 characters
   manifestStartUrl: '/',
-  manifestBackgroundColor: '#663399',
-  manifestThemeColor: '#663399',
+  manifestBackgroundColor: '#126401',
+  manifestThemeColor: '#126401',
   manifestDisplay: 'standalone',
   manifestIcon: 'src/assets/img/website-icon.png',
   pathPrefix: `/Resume/`, // This path is subpath of your hosting https://domain/portfolio
